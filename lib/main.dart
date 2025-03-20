b71961956e0d037c0ca2efbe360d46fdc6caaf4b
Le Hai Dang <danghaile2003@gmail.com>
2025-03-20 18:37:27 +0700

    .
    This reverts commit 01006de8fe09c9f6ae1c645b94503dbbc5ece897.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:ting/Page/home.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-   
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -25,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
